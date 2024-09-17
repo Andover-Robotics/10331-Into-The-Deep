c494804891fc2786e9ad3ebb70c68a00ee2028f1
Anushka <doleanushka@gmail.com>
2024-09-17 19:36:02 -0400

    added camera code, added AprilTagDetectionPipeline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraInit.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+public class CameraInit extends LinearOpMode {
+    //just reuse this code in an actual auto or teleop class, don't use a separate opMode for this
+    OpenCvWebcam webcam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        /*
+         * Instantiate an OpenCvCamera object for the webcam
+         * CameraMonitorViewId is so that the livestream of camera output is displayed on the DS screen
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        webcam.setPipeline(new AprilTagDetectionPipeline()); //set to detect april tags
+
+        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addLine("camera init failed");
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        /*
+         * Wait for the user to press start on the Driver Station
+         */
+        waitForStart();
+
+        while(opModeIsActive()) {
+            sleep(100);
+            //going easy on the CPU
+
+        }
+
+
+    }
+
+}
