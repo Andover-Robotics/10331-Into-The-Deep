728577ff9c6c3db4e7e67d3fe73b05f6632ac81c
Anushka <doleanushka@gmail.com>
2024-09-17 19:09:52 -0400

    added bot, rr, mainteleop, and other gradle stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import org.firstinspires.ftc.teamcode.subsystems.Bot;
+
+import java.lang.Math;
+
+public class MainTeleOp extends LinearOpMode {
+
+    private GamepadEx gp1;
+
+    Bot bot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        bot = Bot.getInstance(this);
+        gp1 = new GamepadEx(gamepad1);
+        drive();
+    }
+
+    private void drive() {
+        gp1.readButtons();
+
+        double driveSpeed = 1;
+
+        driveSpeed *= 1 - 0.9 * gp1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        driveSpeed = Math.max(0, driveSpeed);
+
+        Vector2d driveVector = new Vector2d(gp1.getLeftX(), -gp1.getLeftY()),
+                turnVector = new Vector2d(
+                        gp1.getRightX(), 0);
+
+        bot.driveRobotCentric(-driveVector.component1() * driveSpeed,
+                driveVector.component2() * driveSpeed,
+                turnVector.component1() * driveSpeed / 1.7
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Bot.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Bot {
+
+    //for FSM
+    public enum BotState {
+        INTAKE,
+        TRANSFER,
+        DRIVING,
+        OUTTAKE
+    }
+    public OpMode opMode;
+    public BotState currentState = BotState.DRIVING;
+    public static Bot instance;
+    private final DcMotorEx FL, FR, BL, BR;
+    public boolean fieldCentricRunMode;
+    private double distanceFromBackdrop;
+    private final double optimalDistanceFromBackdrop = 10;
+
+    public static Bot getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("tried to getInstance of Bot when uninitialized");
+        }
+        return instance;
+    }
+
+    public static Bot getInstance(OpMode opMode) {
+        if (instance == null) {
+            return instance = new Bot(opMode);
+        }
+        instance.opMode = opMode;
+        return instance;
+    }
+
+    private Bot(OpMode opMode) {
+        this.opMode = opMode;
+
+        try {
+            fieldCentricRunMode = true;
+        } catch (Exception e) {
+            fieldCentricRunMode = false;
+        }
+
+        FL = opMode.hardwareMap.get(DcMotorEx.class, "fl");
+        FR = opMode.hardwareMap.get(DcMotorEx.class, "fr");
+        BL = opMode.hardwareMap.get(DcMotorEx.class, "bl");
+        BR = opMode.hardwareMap.get(DcMotorEx.class, "br");
+
+        FL.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        BR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        FL.setMode(RUN_USING_ENCODER);
+        FR.setMode(RUN_USING_ENCODER);
+        BL.setMode(RUN_USING_ENCODER);
+        BR.setMode(RUN_USING_ENCODER);
+
+        //subsystems code goes here
+
+    }
+
+    public void driveRobotCentric(double strafeSpeed, double forwardBackSpeed, double turnSpeed) {
+        double[] speeds = {
+                forwardBackSpeed - strafeSpeed - turnSpeed,
+                forwardBackSpeed + strafeSpeed + turnSpeed,
+                forwardBackSpeed + strafeSpeed - turnSpeed,
+                forwardBackSpeed - strafeSpeed + turnSpeed
+        };
+        double maxSpeed = 0;
+        for (int i = 0; i < 4; i++) {
+            maxSpeed = Math.max(maxSpeed, speeds[i]);
+        }
+        if (maxSpeed > 1) {
+            for (int i = 0; i < 4; i++) {
+                speeds[i] /= maxSpeed;
+            }
+        }
+        FL.setPower(speeds[0]);
+        FR.setPower(speeds[1]);
+        BL.setPower(speeds[2]);
+        BR.setPower(speeds[3]);
+
+    }
+
+    public void resetEncoders() {
+        FL.setMode(STOP_AND_RESET_ENCODER);
+        FR.setMode(STOP_AND_RESET_ENCODER);
+        BR.setMode(STOP_AND_RESET_ENCODER);
+        BL.setMode(STOP_AND_RESET_ENCODER);
+    }
+
+}
