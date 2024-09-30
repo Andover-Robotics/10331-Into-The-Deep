c66263b055c3580b24682443b71deaa6c68991ea
anikanagle <nikunagle@gmail.com>
2024-09-30 09:53:58 -0400

    Slides (+ motion profiler stuff)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slides.java
@@ -1,23 +1,169 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
 
 public class Slides {
 
-    private DcMotorEx rightMotor;
-    private DcMotorEx leftMotor;
+    private MotorEx rightMotor;
+    private MotorEx leftMotor;
+    private PIDFController controller;
+    private MotionProfiler profiler = new MotionProfiler(30000, 20000);
+
+
+    //needs to be tuned
+    private final static double p = 0.015, i = 0 , d = 0, f = 0, staticF = 0.25;
+
+    private final double tolerance = 20, powerUp = 0.1, powerDown = 0.05, manualDivide = 1, powerMin = 0.1;
+    private double target = 0 ;
+    public double power;
+    private double manualPower=0;
+    public boolean goingDown=false;
+
+    private double profile_init_time = 0;
+    private final OpMode opMode;
+
+    public enum slidesPosition{
+        GROUND,
+        LOW,
+        MID,
+        HIGH
+    }
+    private slidesPosition position = slidesPosition.GROUND;
+
+    //tune
+    public static int storage =0, top = -2350, mid = -1000, low = -500;
 
     public Slides(OpMode opmode) {
-        rightMotor = opmode.hardwareMap.get(DcMotorEx.class, "slides1");
-        leftMotor= opmode.hardwareMap.get(DcMotorEx.class, "slides2");
-        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor = new MotorEx(opmode.hardwareMap, "slidesRight", Motor.GoBILDA.RPM_312);
+        leftMotor = new MotorEx(opmode.hardwareMap, "slidesCenter", Motor.GoBILDA.RPM_312);
+        rightMotor.setInverted(true);
+        leftMotor.setInverted(false);
+
+        controller = new PIDFController(p, i, d, f);
+        controller.setTolerance(tolerance);
+        controller.setSetPoint(0);
+
+
+        rightMotor.setRunMode(Motor.RunMode.RawPower);
+        rightMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        leftMotor.setRunMode(Motor.RunMode.RawPower);
+        leftMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        this.opMode = opmode;
+
+    }
+
+    public void runTo(double pos) {
+        rightMotor.setRunMode(Motor.RunMode.RawPower);
+        rightMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        leftMotor.setRunMode(Motor.RunMode.RawPower);
+        leftMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        controller = new PIDFController(p, i, d, f);
+        controller.setTolerance(tolerance);
+        resetProfiler();
+        profiler.init(leftMotor.getCurrentPosition(), pos);
+        profile_init_time = opMode.time;
+
+        goingDown = pos > target;
+        target = pos;
+    }
+
+
+    public void runToTop() {
+        runTo(top);
+        position = slidesPosition.HIGH;
+    }
+
+
+    public void runToMid() {
+        runTo(mid);
+        position = slidesPosition.MID;
+    }
+
+
+    public void runToLow() {
+        runTo(low);
+        position = slidesPosition.LOW;
+    }
+
+    public void runToStorage() {
+        position = slidesPosition.GROUND;
+        runTo(storage);
+    }
+
+
+    public void runToManual(double manual) {
+        if (manual > powerMin || manual < -powerMin) {
+            manualPower = manual;
+        } else {
+            manualPower = 0;
+        }
+    }
+
+    public int getCurrentPosition() {
+        return leftMotor.getCurrentPosition();
+    }
+
+    public void resetEncoder() {
+        rightMotor.resetEncoder();
+        leftMotor.resetEncoder();
+    }
+
+
+    public void periodic() {
+
+        //switch this??
+        rightMotor.setInverted(false);
+        leftMotor.setInverted(true);
+        controller.setPIDF(p, i, d, f);
+        double dt = opMode.time - profile_init_time;
+        if (!profiler.isOver()) {
+
+            controller.setSetPoint(profiler.profile_pos(dt));
+            power = powerUp * controller.calculate(leftMotor.getCurrentPosition());
+            if (goingDown) {
+                power = powerDown * controller.calculate(leftMotor.getCurrentPosition());
+            }
+            rightMotor.set(power);
+            leftMotor.set(power);
+
+
+        } else {
+            if (profiler.isDone()) {
+                profiler = new MotionProfiler(30000, 20000);
+               // leftMotor.set(0);
+               // rightMotor.set(0);
+            }
+
+            if (manualPower != 0) {
+
+                controller.setSetPoint(leftMotor.getCurrentPosition());
+                rightMotor.set(manualPower / manualDivide);
+                leftMotor.set(manualPower / manualDivide);
+
+            } else {
+                power = staticF * controller.calculate(leftMotor.getCurrentPosition());
+                rightMotor.set(power);
+                leftMotor.set(power);
+            }
+        }
+    }
+
+    public double getCurrent() {
+        return leftMotor.motorEx.getCurrent(CurrentUnit.MILLIAMPS) + rightMotor.motorEx.getCurrent(CurrentUnit.MILLIAMPS);
+    }
+
+    public void resetProfiler(){
+        profiler = new MotionProfiler(30000, 20000);
     }
 
 }
