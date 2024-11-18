4b58d196cd1e9faa1d26f2545e3ec57078bea3f9
anikanagle <nikunagle@gmail.com>
2024-11-18 17:19:44 -0500

    COLOR SENSOR IS PERFECT! - NO ONE CHANGE THESE VALUES

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ColorSenseTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ColorSenseTest.java
@@ -17,15 +17,11 @@ import org.firstinspires.ftc.teamcode.subsystems.Bot;
 public class ColorSenseTest extends LinearOpMode {
 
     Bot bot;
+
     private GamepadEx gp1;
     private final float[] hsvValues = {0, 0, 0};
-    private double SCALE_FACTOR = 255;
-
     String color="nothing";
     double hue=0, saturation=0, value=0;
-
-
-
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -39,21 +35,28 @@ public class ColorSenseTest extends LinearOpMode {
             telemetry.addLine("Current Color:");
             testColorSensor();
 
-
-             /*   hsvValues[0] => hue
+             /* hsvValues[0] => hue
                 hsvValues[1] => saturation
                 hsvValues[2] => value
-
+                rgb values from image:
+                blue: middle: 32,56,190; highlights:86, 142,238, shadows: 3,5,79
+                red: middle: 218,47,43; highlights:  219, 88, 87, shadows: 112,1,4
+                yellow: middle: 243,175,16; highlights: 241, 193, 76; shadows: 221,107,0
+                hsv:
+                blue: highlights: 218, 65.9%, 93.3%; middle: 231, 83.2, 74.5; shadows: 238, 96.2, 31
+                red: highlights: 0, 60.3, 85.9; middle: 1, 80.3, 85.5; shadows: 358, 99.1, 43.9
+                yellow: highlights: 43, 68.5, 94.5; middle: 42, 93.4, 95.3; shadows: 29, 100, 86.7
               */
 
-            if(hue<180 && hue>89 && saturation<188 && saturation>64 && value>73 && value<255){
+
+            if((hue>10 && hue<80) && (saturation>100 && saturation<160)){
                 color="red";
             }
-            else if(hue>18 && hue<85 && saturation<224 && saturation>77 && value<255 && value>76){
-                color = "yellow";
+            else if((hue>30 && hue<70) && (saturation>140 && saturation<200)){
+                color= "yellow";
             }
-            else if(hue>45 && hue<180 && saturation>0 && saturation<164 && value<200 && value>57){
-                color= "blue";
+            else if((hue>80 && hue<130) && (saturation>80 && saturation<130)){
+                color = "blue";
             }
             else{
                 color= "nothing";
@@ -72,8 +75,8 @@ public class ColorSenseTest extends LinearOpMode {
                 hsvValues);
 
         hue= hsvValues[0];
-        saturation= hsvValues[1] * SCALE_FACTOR;
-        value= hsvValues[2] * SCALE_FACTOR;
+        saturation= hsvValues[1] * 255;
+        value= hsvValues[2] * 255;
 
         telemetry.addData("Hue  ", hue);
         telemetry.addData("Saturation", saturation);
