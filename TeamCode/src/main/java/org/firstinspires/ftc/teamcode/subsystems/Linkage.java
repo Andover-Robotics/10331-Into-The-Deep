ebcfa3493097550439be2aa93a93c220b2a53e62
adole2008 <doleanushka@gmail.com>
2025-03-08 14:53:48 -0500

    actual commit msg: added specimen and linkage fully working teleop RAHHHHH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linkage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linkage.java
@@ -13,13 +13,13 @@ public class Linkage {
 
     public static final double max_1 = 0.42;
     public static final double abs_min_1 = 0.09;
-    public static final double min_1 = 0.145;
+    public static final double min_1 = 0.14;
     //original: 0.09
     //new orig: 0.145
 
     public static final double max_2 = 0.39;
     public static final double abs_min_2 = 0.14;
-    public static final double min_2 = 0.195;
+    public static final double min_2 = 0.19;
     //original: 0.14
     //new orig: 0.195
 
